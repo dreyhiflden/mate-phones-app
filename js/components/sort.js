f7199d65149d8c9a7be79665bf7ed799212164be
dreyhiflden <dreyhiflden@gmail.com>
2019-03-07 01:29:24 +0200

    Implement sort

--- js/components/sort.js
+++ js/components/sort.js
@@ -1,6 +1,12 @@
 import BaseComponent from "./base-component.js";
 
 class Sort extends BaseComponent {
+    constructor(params) {
+        super(params);
+
+        this._parent = params.parent;
+    }
+
     _render() {
         this._element.innerHTML = `
             <p>
@@ -11,6 +17,16 @@ class Sort extends BaseComponent {
                 </select>
               </p>
       `;
+
+        this.addListeners()
+    }
+
+    addListeners() {
+        this._element.querySelector('select')
+          .addEventListener('change', (event) => {
+                this._parent.sortUpdated(event.currentTarget.value);
+            }
+          );
     }
 }
 
--- js/services/phone-service.js
+++ js/services/phone-service.js
@@ -11,6 +11,26 @@ class PhoneService {
         });
     }
 
+    static getSorted(value) {
+        if (value === 'age') {
+            return phones.sort((a, b) => a.age - b.age);
+        }
+
+        if (value === 'name') {
+            return phones.sort((a, b) => {
+                if (a.name < b.name) {
+                    return -1;
+                }
+
+                if (a.name > b.name) {
+                    return 1;
+                }
+
+                return 0;
+            });
+        }
+    }
+
     static async getPhone(id) {
         return (await import(`../../phones/${id}.js`)).default;
     }
